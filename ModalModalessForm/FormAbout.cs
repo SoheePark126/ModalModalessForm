5926b07be899e00f2f9d60936e7235a0b5117882
Sohee Park <qkrthgml126@naver.com>
2019-06-25 11:21:20 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3606 박소희

--- ModalModalessForm/FormAbout.cs
+++ ModalModalessForm/FormAbout.cs
@@ -15,5 +15,12 @@ namespace ModalModalessForm
         {
             InitializeComponent();
         }
+
+        public string Title
+        {
+            set { this.Text = value; }
+        }
+        
+
     }
 }
